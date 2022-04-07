96e3557853c4379c26b1ac4524cce9c154f0cc6f
Chinwike <maduabuchichizzy@gmail.com>
2022-04-07 15:25:08 +0100

    react-router-dom setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,31 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <div className='App'>
+    /* 
+      v6 changes
+      - Route component should be wrapped with Routes component
+      - component prop on Route has been replaced with element. element accepts jsx
+      - Redirect component has been replaced with Navigate component
+      - useLocation & useNavigate hooks available
+    */
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   )
 }
 
